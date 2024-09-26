7a52c3dcb51ccdf9fbf544653295f8ad67f623da
flutter-lord <richardolojede2@gmail.com>
2024-09-26 17:24:08 +0100

    commit

--- bin/Pattern.dart
+++ bin/Pattern.dart
@@ -5,8 +5,21 @@ void main() {
   stdout.write('Enter any digit from 1 to 9: ');
   int digit = int.parse(stdin.readLineSync()!);
 
-  numberPattern(digit);
- 
+  for (int line = 1; line <= digit; line++) {
+
+    for (int space = 2 * (digit - line); space > 0; space--) {
+      stdout.write(' ');
+    }
+
+    for (int i = line; i > 0; i--) {
+      
+        stdout.write('$i ');
+    }
+
+      print('');
+  }
+
+  //numberPattern(digit);
 }
 
 void numberPattern(int digit) {
@@ -16,14 +29,8 @@ void numberPattern(int digit) {
      for (int space = 2 * (digit - i); space > 0; space--) {
       stdout.write(' ');
      }
-
-
       stdout.write('$i ');
       print('');
     }
-
-  
   }
-  
-
 }
