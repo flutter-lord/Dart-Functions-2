3578e3fc8697eb00a65f95b051402488cb1b13b2
flutter-lord <richardolojede2@gmail.com>
2024-09-27 22:01:25 +0100

    commit

---
+++ bin/PhoneKeyped.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+
+  stdout.write('Enter a string: ');
+  String string = stdin.readLineSync()!;
+
+  phoneNumber(string);
+
+}
+void phoneNumber (String string) {
+
+  
+}
