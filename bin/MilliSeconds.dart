462f339fee027ce129b32aad3e7f009a52009d1f
flutter-lord <richardolojede2@gmail.com>
2024-09-29 14:34:32 +0100

    commit

--- bin/MilliSeconds.dart
+++ bin/MilliSeconds.dart
@@ -1,29 +1,21 @@
-import 'dart:io';
+ import 'dart:io';
 
 void main() {
 
-  stdout.write('Enter any value for milliseconds: ');
-  int milliseconds = int.parse(stdin.readLineSync()!);
+  stdout.write('Enter any value for milliSecond: ');
+  int milliSecond = int.parse(stdin.readLineSync()!);
 
-  int  seconds = milliseconds ~/ 1000;
-  int minutes = seconds * 60;
-  int hour = minutes * 60;
-
-  int a = milliseconds % hour;
-  int b = a % minutes;
-  int c = b % seconds;
-
-  print('$milliseconds is $a : $b : $c');
-
+  convertMilliSeconds(milliSecond);
 
 }
 
-int millisecondsToSeconds(int milliseconds, int c) {
-  return c;
-}
-int millisecondsToMinutes (int milliseconds, int b) {
-  return b; 
-}
-int millisecondsToHour(int milliseconds, int a) {
-  return a;
+void convertMilliSeconds(int milliSecond) {
+
+  int timeInSecond = milliSecond ~/ 1000;
+  int hour = timeInSecond ~/ 3600;
+  int minute = (timeInSecond ~/ 60) % 60;
+  num second = timeInSecond % 60;
+  
+
+  print('$milliSecond milliSeconds is $hour hour: $minute minute: $second second');
 }
