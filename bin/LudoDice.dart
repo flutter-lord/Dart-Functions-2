89c217a64623d00e2d2ee939d6bee659d1eb2a16
flutter-lord <richardolojede2@gmail.com>
2024-10-02 22:34:31 +0100

    commit

--- bin/LudoDice.dart
+++ bin/LudoDice.dart
@@ -1,9 +1,9 @@
 import 'dart:math';
 
 void main() {
-  int random1 = Random().nextInt(12) + 1;
+  int random1 = Random().nextInt(6) + 1;
 
-  int random2 = Random().nextInt(12) + 1;
+  int random2 = Random().nextInt(6) + 1;
 
   LudoGame(random1, random2);
 
@@ -12,17 +12,17 @@ void LudoGame(int random1, int random2) {
   int result = random1 + random2;
 
   if (result == 2 || result == 3 || result == 12) {
-    print('You Lose');
+    print('You rolled ${random1 +random2} = ${result}\nYou Lose');
   }
   else if (result == 7 || result == 11) {
-    print('You Won');
+    print('You rolled $random1 + $random2 = $result\nYou Won');
   }
   else {
     if (result == 7)  {
-      print('Continue to roll\nYou lose');
+      print('You rolled $random1 + $random2 = $result\nPoint is $result\nYou lose');
     }
     else {
-      print('Continue to roll\nYou Won');
+      print('You rolled $random1 + $random2 = $result\nPoint is $result\nYou Won');
     }
   }
 } 
