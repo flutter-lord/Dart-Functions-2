e76e1e46f8887bfe75305492fb764fc8f7915ad9
flutter-lord <richardolojede2@gmail.com>
2024-10-02 19:42:07 +0100

    commit

--- bin/TwinPrime.dart
+++ bin/TwinPrime.dart
@@ -1,25 +1,18 @@
-import 'dart:io';
-
 import 'PaliddromePrime.dart';
 
 void main() {
 
-  for (int i = 1 ; i <= 1000; i++) {
-    twinsPrime(i);
+  for (int i = 1; i <= 1000; i++) {
+    twinsPrime(i);  
   }
   
 }
 void twinsPrime(int i) {
-  int count = 0;
+
   bool prime = isPrime(i);
 
- if (count == 2) {
-  print('');
- }
-
  if (prime == true) {
-  stdout.write('$i ');
-  count++;
+  print('($i, ${i++})');
  }
 
 }
